631dd2a62ac83e5eeb8a9a2671495fe5019c508a
taogeT <zwtzjd@gmail.com>
2016-03-27 10:39:41 +0800

    init module detail

---
+++ README.md
@@ -0,0 +1,6 @@
+# Flask Celery Integration
+Python: 3.3+ CELERY: 3.0++
+
+Celery: http://celeryproject.org
+
+Using Flask with Celery
---
+++ setup.py
@@ -0,0 +1,44 @@
+# -*- coding: UTF-8 -*-
+"""
+Flask-Celery
+-------------
+
+Flask supports for Module Celery.
+
+"""
+from setuptools import setup
+
+version = '0.0.1'
+
+setup(
+    name='Flask-Celery',
+    version=version,
+    url='https://github.com/taogeT/flask-celery',
+    license='MIT',
+    author='Zheng Wentao',
+    author_email='zwtzjd@gmail.com',
+    description='Celery 3.0+ integration for Flask',
+    long_description=__doc__,
+    py_modules=['flask_celery'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'Flask>=0.8',
+        'Flask-Script>=2.0.0',
+        'celery>=3.0.0',
+    ],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Environment :: Web Environment',
+        'Framework :: Flask',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
