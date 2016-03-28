7924872b1e6286200e26777aa60e811c85ae4680
taogeT <zwtzjd@gmail.com>
2016-03-28 10:40:41 +0800

    Upgrade CeleryCommand, from function to class.

--- flask_celery.py
+++ flask_celery.py
@@ -1,9 +1,11 @@
 # -*- coding: UTF-8 -*-
-from celery.app import App
+from celery import Celery as _Celery
 from celery.bin.celery import CeleryCommand as _CeleryCommand
 from flask.ext.script import Command, Option, Manager
 
 import argparse
+
+__all__ = ['Celery', 'CeleryCommand']
 
 
 class _CeleryConfig(object):
@@ -12,10 +14,10 @@ class _CeleryConfig(object):
         self.celery = celery
 
 
-class Celery(App):
+class Celery(_Celery):
 
     def __init__(self, app=None, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+        super(_Celery, self).__init__(*args, **kwargs)
         if app is not None:
             self.init_app(app)
 
@@ -29,78 +31,61 @@ class Celery(App):
         return super().__reduce_args__()
 
 
-def option_optparse_to_argparse(opt_option):
-    opt_option_dict = vars(opt_option)
-    # args name_or_flag
-    arg_args = opt_option_dict['_short_opts'] + opt_option_dict['_long_opts']
-    # kwargs
-    arg_kwargs = {}
-    # dest
-    arg_kwargs['dest'] = opt_option_dict['dest']
-    # help
-    arg_kwargs['help'] = opt_option_dict['help']
-    # nargs
-    arg_kwargs['nargs'] = opt_option_dict['nargs']
-    # const
-    arg_kwargs['const'] = opt_option_dict['const']
-    # default
-    if opt_option_dict['default'] != ('NO', 'DEFAULT'):
-        arg_kwargs['default'] = opt_option_dict['default']
-    # type
-    type_transfer = {
-        'string': str,
-        'int': int,
-        'choice': str,
-        'float': float,
-        'complex': complex,
-    }
-    if opt_option_dict['type']:
-        arg_kwargs['type'] = type_transfer[opt_option_dict['type']]
-    else:
-        arg_kwargs['type'] = None
-    # choices
-    arg_kwargs['choices'] = opt_option_dict['choices']
-    # metavar
-    arg_kwargs['metavar'] = opt_option_dict['metavar']
-    # action
-    arg_kwargs['action'] = opt_option_dict['action']
-    if arg_kwargs['action'] == 'store_true':
-        for key in ['const', 'type', 'nargs', 'metavar', 'choices']:
-            arg_kwargs.pop(key)
-    elif arg_kwargs['action'] == 'store':
-        arg_kwargs.pop('nargs')
-    if arg_kwargs['action'] == 'callback':
-        callback_func = opt_option_dict['callback']
-        callback_args = opt_option_dict['callback_args']
-        callback_kwargs = opt_option_dict['callback_kwargs']
-
-        class _action_cls(argparse.Action):
-
-            def __call__(self, parser, namespace, values, option_string=None):
-                return callback_func(*callback_args, **callback_kwargs)
-
-        arg_kwargs['action'] = _action_cls
-        arg_kwargs['nargs'] = 0
-
-    return Option(*arg_args, **arg_kwargs)
-
-
-class TransferCommand(Command):
+class _TransferCommand(Command):
 
     def __init__(self, celery_command):
-        super().__init__()
+        super(Command, self).__init__()
         self.celery_command = celery_command
         for celery_option in list(celery_command.get_options()):
-            transfer_option = option_optparse_to_argparse(celery_option)
-            self.add_option(transfer_option)
+            self.add_option(self._transfer_option(celery_option))
+
+    def _transfer_option(self, opt_option):
+        option_kwargs = vars(opt_option)
+        # args name_or_flag
+        option_args = option_kwargs.pop('_short_opts') + option_kwargs.pop('_long_opts')
+        # default
+        if option_kwargs['default'] == ('NO', 'DEFAULT'):
+            option_kwargs['default'] = None
+        # type
+        type_transfer = {
+            'string': str,
+            'int': int,
+            'choice': str,
+            'float': float,
+            'complex': complex,
+        }
+        if option_kwargs['type']:
+            option_kwargs['type'] = type_transfer.get(option_kwargs['type'], option_kwargs['type'])
+        # action
+        if option_kwargs['action'] == 'store_true':
+            for key in ['const', 'type', 'nargs', 'metavar', 'choices']:
+                option_kwargs.pop(key)
+        elif option_kwargs['action'] == 'store':
+            option_kwargs.pop('nargs')
+        if option_kwargs['action'] == 'callback':
+            callback_func = option_kwargs.pop('callback')
+            callback_args = option_kwargs.pop('callback_args')
+            callback_kwargs = option_kwargs.pop('callback_kwargs')
+
+            class _action_cls(argparse.Action):
+
+                def __call__(self, parser, namespace, values, option_string=None):
+                    return callback_func(*callback_args, **callback_kwargs)
+
+            arg_kwargs['action'] = _action_cls
+            arg_kwargs['nargs'] = 0
+
+        return Option(*option_args, **option_kwargs)
 
     def run(self, **kwargs):
         self.celery_command.run(**kwargs)
 
 
-def CeleryCommand(app):
-    command_manager = Manager(usage='Perform celery command')
-    for name, command_class in _CeleryCommand.commands.items():
-        command_class_instance = command_class(app=app)
-        command_manager.add_command(name, TransferCommand(command_class_instance))
-    return command_manager
+class CeleryCommand(Manager):
+
+    def __init__(self, celery):
+        super(Manager, self).__init__(usage='Perform celery command')
+        for name, command_class in _CeleryCommand.commands.items():
+            command_class_instance = command_class(app=celery)
+            self.add_command(name, _TransferCommand(command_class_instance))
+
