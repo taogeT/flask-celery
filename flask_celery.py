0b165595c934595a79b0b7f972e6d5fe42691825
taogeT <zwtzjd@gmail.com>
2016-05-31 01:11:41 +0800

    修改参数获取，保留原celery参数顺序

--- flask_celery.py
+++ flask_celery.py
@@ -4,18 +4,12 @@ from celery import Celery as _Celery
 __all__ = ['Celery']
 
 
-class _CeleryConfig(object):
-
-    def __init__(self, celery):
-        self.celery = celery
-
-
 class Celery(_Celery):
 
-    def __init__(self, app=None, **kwargs):
+    def __init__(self, **kwargs):
         super(Celery, self).__init__(**kwargs)
-        if app is not None:
-            self.init_app(app)
+        if 'app' in kwargs:
+            self.init_app(kwargs['app'])
 
     def init_app(self, app):
         self.conf.update(app.config)
@@ -23,6 +17,3 @@ class Celery(_Celery):
             self.conf.update(BROKER_URL=app.config['CELERY_BROKER_URL'])
         if 'CELERY_ADMINS' in app.config:
             self.conf.update(ADMINS=app.config['CELERY_ADMINS'])
-        if not hasattr(app, 'extensions'):
-            app.extensions = {}
-        app.extensions['celery'] = _CeleryConfig(self)
